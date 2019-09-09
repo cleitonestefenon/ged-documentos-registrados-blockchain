c82f646d18b36f392ed5888ef96405688e81f301
clediano <cledianoestefenon@gmail.com>
2019-09-09 19:17:40 -0300

    ajustando questao do avatar aceitar qualquer tipo de img

--- src/common/functions/index.js
+++ src/common/functions/index.js
@@ -6,15 +6,15 @@ import { KEY_STORAGE } from 'common/localstorage/const';
 export const loadAvatar = (avatarId, onSuccess) => {
     api.get(`${FILE_SERVICE}/file/${avatarId}`)
         .then(resp => {
-            saveAsSessionStorage(KEY_STORAGE.AVATAR, `data:image/png;base64,${btoa(String.fromCharCode(...new Uint8Array(resp.data.file.data)))}`)
-            onSuccess && onSuccess(`data:image/png;base64,${btoa(String.fromCharCode(...new Uint8Array(resp.data.file.data)))}`);
+            saveAsSessionStorage(KEY_STORAGE.AVATAR, `data:${resp.mimetype};base64,${btoa(String.fromCharCode(...new Uint8Array(resp.data.file.data)))}`)
+            onSuccess && onSuccess(`data:${resp.mimetype};base64,${btoa(String.fromCharCode(...new Uint8Array(resp.data.file.data)))}`);
         })
 }
 
 export const mountDataImage = async avatarId => {
     const resp = await api.get(`${FILE_SERVICE}/file/${avatarId}`)
 
-    return `data:image/png;base64,${btoa(String.fromCharCode(...new Uint8Array(resp.data.file.data)))}`;
+    return `data:${resp.mimetype};base64,${btoa(String.fromCharCode(...new Uint8Array(resp.data.file.data)))}`;
 }
 
 export const removeElementOfList = (list, simple = true, field, valueOfElement) => {
@@ -22,7 +22,6 @@ export const removeElementOfList = (list, simple = true, field, valueOfElement) 
         if (simple) {
             return element !== valueOfElement;
         } else {
-            console.log(element[field] + ' - ' + valueOfElement)
             return element[field] !== valueOfElement;
         }
     })
